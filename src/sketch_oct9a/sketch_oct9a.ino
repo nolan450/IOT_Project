ef21201d90d778a5ff018a9bd61c1f637a54a477
DESKTOP-490CFTI\Admin <nolan974pro@gmail.com>
2024-10-09 16:46:00 +0200

    initialisation variable broche

---
+++ src/sketch_oct9a/sketch_oct9a.ino
@@ -0,0 +1,25 @@
+int yellowLedPin = 12;
+int humiditySensorPin = 17;
+int steamSensorPin = 34;
+int fanDirectionPin = 18;
+int fanPowerPin = 19;
+int pirMotionSensorPin = 14;
+int leftButtonPin = 16;
+int rightButtonPin = 27;
+int rgbPin = 26;
+int gasSensor = 23;
+int buzzer = 25;
+int windows = 5;
+int door = 13;
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  
+}
