aeac14c29860c78d0e460d2ebd4f16758068fcb2
Manzone Florian <manzoneflorianpro@gmail.com>
2024-10-10 16:43:10 +0200

    feat: add blonking leds, opening window and buzzer melody when gas is detected

--- src/GasDetectionAlert/GasDetectionAlert.ino
+++ src/GasDetectionAlert/GasDetectionAlert.ino
@@ -1,52 +1,86 @@
 /*
 Gas Detection Alert
 
-Si du gaz est détecté, le ventilateur s'active, le buzzer sonne pour donner l'alerte et un message apparait sur l'écran LCD
+Si du gaz est détecté, le ventilateur s'active, le buzzer sonne pour donner l'alerte et un message apparait sur l'écran LCD.
+La LED RGB clignote en rouge.
 */
-
-int rgbPin = 26;
-int gasSensor = 23;
-int buzzer = 25;
-int windows = 5;
-int fanDirectionPin = 18;
-int fanPowerPin = 19;
 
 #include <Wire.h>
 #include <ESP32Servo.h>
 #include <LiquidCrystal_I2C.h>
+#include <Adafruit_NeoPixel.h>
+
+#define RGB_PIN 26
+#define NUM_LEDS 4
+#define GAS_SENSOR_PIN 23
+#define BUZZER_PIN 25
+#define WINDOWS_PIN 5
+#define FAN_POWER_PIN 19
 
 LiquidCrystal_I2C lcd(0x27, 16, 2); 
 Servo ServoWindows;
+Adafruit_NeoPixel leds = Adafruit_NeoPixel(NUM_LEDS, RGB_PIN, NEO_GRB + NEO_KHZ800);
+
 int pos = 0;
 
 void setup() {
-  ServoWindows.attach(windows);
-  pinMode(gasSensor, INPUT);
-  pinMode(buzzer, OUTPUT);
-  pinMode(rgbPin, OUTPUT);
-  pinMode(fanPowerPin, OUTPUT);
-
+  ServoWindows.attach(WINDOWS_PIN);
+  pinMode(GAS_SENSOR_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+  pinMode(FAN_POWER_PIN, OUTPUT);
+  
   Serial.begin(9600);
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(3, 0);
+  lcd.setCursor(2, 0);
   lcd.print("Tout va bien");
 
+  leds.begin();
+  leds.show();
+  ServoWindows.write(0);
 }
 
 void loop() {
-  Serial.println(digitalRead(gasSensor));
-  if(digitalRead(gasSensor) == 0){
-    tone(buzzer, 589);
-    digitalWrite(fanPowerPin, HIGH);
+  int gasDetected = digitalRead(GAS_SENSOR_PIN);
+
+  if (gasDetected == 0) {
+    digitalWrite(FAN_POWER_PIN, HIGH);
+    ServoWindows.write(120);
+    blinkingLeds();
     lcd.clear();
+    lcd.setCursor(4, 0);
     lcd.print("Alerte !");
-    lcd.setCursor(3, 1);
+    lcd.setCursor(1, 1);
     lcd.print("Fuite de gaz !");
-    delay(300);
-  }else {
-    noTone(buzzer);
-    digitalWrite(fanPowerPin, LOW);
+    for(int i = 0; i < 1; i++){
+      tone(BUZZER_PIN, 589); 
+      delay(500);
+      tone(BUZZER_PIN, 700); 
+      delay(500);
+    }
+  } else {
+    noTone(BUZZER_PIN);
+    digitalWrite(FAN_POWER_PIN, LOW);
+    
+    leds.clear();
+    leds.show();
+    ServoWindows.write(0);
+
+    lcd.clear();
+    lcd.setCursor(2, 0);
+    lcd.print("Tout va bien");
   }
 
+  delay(500);
+}
+
+void blinkingLeds() {
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds.setPixelColor(i, leds.Color(255, 0, 0));
+  }
+  leds.show();
+  delay(5);
+  leds.clear();
+  leds.show();
+  delay(5);
 }
